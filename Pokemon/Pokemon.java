4f9dfda24b3d909802c91e3ce17e025ed14ffe2c
EnzoFavret3il <favrete@3il.fr>
2023-12-05 11:48:45 +0100

    poke2

--- Pokemon/Pokemon.java
+++ Pokemon/Pokemon.java
@@ -1,5 +1,35 @@
 package Pokemon;
 
 public class Pokemon {
-
+	private String nom;
+	private double pv;
+	private double atk;
+	public Pokemon(String nom, double pv, double atk) {
+		super();
+		this.nom = nom;
+		this.pv = pv;
+		this.atk = atk;
+	}
+	public String getNom() {
+		return this.nom;
+	}
+	public double getPv() {
+		return this.pv;
+	}
+	public double getAtk() {
+		return this.atk;
+	}
+	
+	public boolean estKO() {
+		if(this.pv==0) {
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	public void attaquer(Pokemon cible) {
+		cible.pv=cible.pv-this.atk;
+	}
+	
 }
