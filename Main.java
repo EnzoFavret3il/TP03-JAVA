daf69bdd760be60b8dbbc012f807ba7798987427
EnzoFavret3il <favrete@3il.fr>
2023-12-05 09:44:06 +0100

    Exo 1.2

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* Exo 1.1
 		Point a= new Point();
 		a.initialise(1, 2);
 		PointA b = new PointA();
 		b.initialise(1,2);
-		b.afficher();
+		b.afficher();*/
+		
+		/*Exo 1.2*/
+		PointNom test=new PointNom(5,6,"LETEST");
+		test.setPointNom(9, 8, "LETEST2");
+		test.setNom("LETEST3");
+		test.affCoordNom();
 	}
 
 }
--- PointNom.java
+++ PointNom.java
@@ -1,4 +1,24 @@
 
-public class PointNom {
-
+public class PointNom extends Point{
+	private int x;
+	private int y;
+	private String Nom;
+	
+	public PointNom(int x, int y, String nom) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.Nom = nom;
+	}
+	public void setPointNom(int x, int y, String nom) {
+	this.x = x;
+	this.y = y;
+	this.Nom = nom;
+	}
+	public void setNom(String nom) {
+		this.Nom=nom;
+	}
+	public void affCoordNom() {
+		System.out.print("coordonnées: "+this.x +" "+this.y+ " Voici le nom: "+ this.Nom);
+	}
 }
